c3d93066c9413074e7fca1c3246c95d6f5f79998
KoeInoue <kjustininoue1996@gmail.com>
2023-01-20 20:40:48 +0900

    #1 create customer

---
+++ config.go
@@ -0,0 +1,26 @@
+package square
+
+type Sandbox string
+type Production string
+
+// Environment has part of the domain that varies with the environment.
+type Env struct {
+    Sandbox Sandbox
+    Production Production
+}
+
+// Config struct contains access token and environment
+type Config struct {
+    AccessToken string
+    Environment Env
+}
+
+const (
+    ENV_SANDBOX = Sandbox("https://connect.squareupsandbox.com")
+    ENV_PRODUCTION = Production("https://connect.squareup.com")
+)
+// Environment has part of the domain that varies with the environment.
+var Environment = Env{
+    Sandbox: ENV_SANDBOX,
+    Production: ENV_PRODUCTION,
+}
